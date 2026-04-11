17ce88d3ab5d3aeb78749ad34e72ab4cf65ae1df
DanGosw <dannydospuntosv@gmail.com>
2026-04-11 09:58:38 -0500

    Comment out event section in `webEvent` page for temporary removal.

--- src/pages/public/webEvent/index.vue
+++ src/pages/public/webEvent/index.vue
@@ -20,18 +20,18 @@ import TimerEventPage from "@/pages/public/webEvent/TimerEventPage.vue";
         <PricingSection v-reveal/>
         <LocationEvent v-reveal/>
 
-        <div class="max-w-4xl mx-auto text-center bg-event-bg py-16 px-4" v-reveal>
-            <div class="w-32 h-1 bg-event-accent mx-auto mb-6 rounded-full"></div>
+        <!--        <div class="max-w-4xl mx-auto text-center bg-event-bg py-16 px-4" v-reveal>-->
+        <!--            <div class="w-32 h-1 bg-event-accent mx-auto mb-6 rounded-full"></div>-->
 
-            <h2 class="text-3xl md:text-5xl font-info text-white leading-tight">
-                KADOSH
-                <span class="block text-event-accent mt-2"> "Llamados a Santidad" </span>
-            </h2>
+        <!--            <h2 class="text-3xl md:text-5xl font-info text-white leading-tight">-->
+        <!--                KADOSH-->
+        <!--                <span class="block text-event-accent mt-2"> "Llamados a Santidad" </span>-->
+        <!--            </h2>-->
 
-            <p class="mt-6 text-white/60 text-sm md:text-base max-w-2xl mx-auto">
-                Un llamado a vivir apartados, con propósito y entrega total a Dios.
-            </p>
-        </div>
+        <!--            <p class="mt-6 text-white/60 text-sm md:text-base max-w-2xl mx-auto">-->
+        <!--                Un llamado a vivir apartados, con propósito y entrega total a Dios.-->
+        <!--            </p>-->
+        <!--        </div>-->
         <TeamMembersEvent v-reveal/>
         <FooterPage/>
     </main>
